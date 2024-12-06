dfbba3e765adc7d563f17c67c841483536966424
Tanaka878 <musungaretanaka@gmail.com>
2024-12-06 16:33:39 +0200

    implementing security

--- src/main/java/org/example/communityforumapp/config/JwtAuthenticationFilter.java
+++ src/main/java/org/example/communityforumapp/config/JwtAuthenticationFilter.java
@@ -1,4 +1,34 @@
 package org.example.communityforumapp.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request
+            , @NonNull HttpServletResponse response,
+                                   @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String AuthHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (AuthHeader != null && AuthHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt = AuthHeader.substring(7);
+
+
+    }
 }
