f87958ada154bfd96091574bc6391cefda2dbe2d
Tanaka878 <musungaretanaka@gmail.com>
2024-12-14 20:16:27 +0200

    Add firstName field to User and update Hibernate config
    Introduced a new field `firstName` in the `User` entity to store user first names. Adjusted Hibernate configuration in `application.properties` to use `update` for schema changes instead of `create-drop`.

--- src/main/java/org/example/communityforumapp/user/User.java
+++ src/main/java/org/example/communityforumapp/user/User.java
@@ -25,6 +25,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String firstName;
     private String username;
     private String password;
     private String email;
